6d28cd4a4306159e29c8fca9f71b4f08ed8d9959
Chandresh Makwana <Chandresh.Makwana@harman.com>
2018-07-17 10:02:48 +0530

    Added the solution.

---
+++ TurtleAttack/Controllers/PanelController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TurtleAttack.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Panel")]
+    public class PanelController : Controller
+    {
+    }
+}
