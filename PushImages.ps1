7da08e73b4237804380530aba6cfdc5dd3bcf6cc
Chandresh Makwana <Chandresh.Makwana@harman.com>
2018-07-19 02:22:31 +0530

    Added version upgrade.

--- PushImages.ps1
+++ PushImages.ps1
@@ -1,3 +1,3 @@
 ﻿docker login --username cjmakwana --password Sparcnet@78H
-docker tag turtleattack:1.0.2 cjmakwana/turtleattack:1.0.2
-docker push cjmakwana/turtleattack:1.0.2
+docker tag turtleattack:1.0.3 cjmakwana/turtleattack:1.0.3
+docker push cjmakwana/turtleattack:1.0.3
